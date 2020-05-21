c2707dc81887bad72a76b20c9bd880283fbbe689
filip <filip.milkovic1@gmail.com>
2020-05-21 16:10:22 +0200

    Riješeno

---
+++ RPPOON LV4/ZAD1/Adapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD1
+{
+    class Adapter : IAnalytics
+    {
+        private Analyzer3rdParty analyticsService;
+        public Adapter(Analyzer3rdParty service)
+        {
+            this.analyticsService = service;
+        }
+        private double[][] ConvertData(Dataset dataset)
+        {
+            IList<List<double>> data = dataset.GetData();
+            double[][] convertedData = new double[data.Count][];
+            foreach(List<double> line in data)
+            {
+                for(int i = 0; i < data.Count; i++)
+                {
+                    convertedData[i] = new double[data[i].Count];
+                    data[i].CopyTo(convertedData[i]);
+                }
+            }
+            return convertedData;
+        }
+        public double[] CalculateAveragePerColumn(Dataset dataset)
+        {
+            double[][] data = this.ConvertData(dataset);
+            return this.analyticsService.PerColumnAverage(data);
+        }
+        public double[] CalculateAveragePerRow(Dataset dataset)
+        {
+            double[][] data = this.ConvertData(dataset);
+            return this.analyticsService.PerRowAverage(data);
+        }
+    }
+}
---
+++ RPPOON LV4/ZAD2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset file = new Dataset(@"E:\Projects\GitHub\RPPOON-LV4\RPPOON LV4\ZAD2\csv.csv");
+
+            IAnalytics analytics = new Adapter(new Analyzer3rdParty());
+
+            Console.WriteLine("ROW AVERAGE");
+            foreach(double average in analytics.CalculateAveragePerRow(file))
+            {
+                Console.WriteLine(average);
+            }
+            Console.WriteLine("COLUMN AVERAGE");
+            foreach (double average in analytics.CalculateAveragePerColumn(file))
+            {
+                Console.WriteLine(average);
+            }
+        }
+    }
+}
